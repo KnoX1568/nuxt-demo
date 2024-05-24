08b4a8965cb9906447c29ef46978026602e97443
KnoX1568 <neofuenf+gitlab@googlemail.com>
2024-05-24 21:52:33 +0200

    Added vuetify and layout + light-/ darkmode-toggle

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer" />
+      <v-toolbar-title text="Demo" />
+      <v-spacer />
+      <v-btn @click="toggleTheme" :icon="(theme.global.current.value.dark) ? 'mdi-weather-sunny' : 'mdi-moon-waxing-crescent'" />
+    </v-app-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        :location="$vuetify.display.mobile ? 'top' : undefined"
+    >
+      <v-list>
+        <v-list-item title="Menu" />
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px">
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+const drawer = ref<boolean>(false)
+
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
